42eab64f136a75c5dc82d056ed78a070e7003371
Alexsander Rene <alexrene742oliveira@gmail.com>
2024-05-07 16:44:04 -0400

    Adicionando a pasta Algoritmos e Estruturas de Dados, com duas implementações uma de fila e outra de pilha

---
+++ Estruturas de Dados/Fila/fila.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+typedef struct Celula_Fila
+{
+    float elemento;
+    struct Celula_Fila *proximo_ponteiro;
+}Celula_Fila;
+
+typedef struct Fila
+{
+    Celula_Fila *primeiro, *ultimo;
+}Fila;
+
+bool Lista_Vazia(Fila *fila)
+{
+    if(fila-> primeiro == NULL)
+        return true;
+    return false;
+}
+
+void inicializar_fila(Fila *fila)
+{
+    fila->primeiro = NULL;
+    fila->ultimo = NULL;
+}
+
+bool adicionar_elemento_na_fila(Fila *fila, float elemento)
+{
+    Celula_Fila *nova_celula_da_fila = (Celula_Fila*)malloc(sizeof(Celula_Fila));
+    nova_celula_da_fila->elemento = elemento;
+    nova_celula_da_fila->proximo_ponteiro = NULL;
+
+    if(Lista_Vazia(fila))
+    {
+        fila->primeiro = nova_celula_da_fila;
+        fila->ultimo = nova_celula_da_fila;
+        return true;
+    }
+    else
+    {
+        fila->ultimo->proximo_ponteiro = nova_celula_da_fila;
+        fila->ultimo = nova_celula_da_fila;
+        return true;
+    }
+    return false;
+}
+
+Celula_Fila* remover_elemento_na_fila(Fila *fila)
+{
+    Celula_Fila *temporario = fila->primeiro;
+    fila->primeiro = fila->primeiro->proximo_ponteiro;
+    return temporario;
+}
+
+void percorrer_elementos_na_fila(Fila *fila)
+{
+    Celula_Fila *temporario = fila->primeiro;
+    while(temporario != NULL)
+    {
+        printf("Elemento: %.2f\n",temporario->elemento);
+        temporario = temporario->proximo_ponteiro;
+    }
+}
+
+int main()
+{
+
+    Fila fila;
+    inicializar_fila(&fila);
+    int opcao;
+    float valor;
+
+    do
+    {
+        printf("------------------Bem-Vindo------------------------\n");
+        printf("------------------Fila  em C-----------------------\n");
+        printf("1-Adicionar Elemento na Fila\n");
+        printf("2-Remover um Elemento na Fila\n");
+        printf("3-Percorrer os Elementos na Fila\n");
+        printf("0-Sair\n\n");
+
+        scanf("%d",&opcao);
+
+        switch(opcao)
+        {
+            case 1:
+                printf("Elemento a ser adicionado na fila: ");
+                scanf("%f",&valor);
+                adicionar_elemento_na_fila(&fila, valor);
+                break;
+            case 2:
+                remover_elemento_na_fila(&fila);
+            case 3:
+                percorrer_elementos_na_fila(&fila);
+        }
+    } while (opcao != 0);
+    
+    return EXIT_SUCCESS;
+}
---
+++ Estruturas de Dados/Pilha/pilha.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+typedef struct Celula_Pilha
+{
+    float elemento;
+    struct Celula_Pilha *proximo_ponteiro;
+}Celula_Pilha;
+
+typedef struct Pilha
+{
+    Celula_Pilha *topo;
+}Pilha;
+
+bool pilha_vazia(Pilha *pilha)
+{
+    if(pilha->topo == NULL)
+        return true;
+    return false;
+}
+
+void inicializar_pilha(Pilha *pilha)
+{
+    pilha->topo = NULL; 
+}
+
+bool adicionar_elemento_na_pilha(Pilha *pilha, float elemento)
+{
+    Celula_Pilha *nova_celula_da_pilha = (Celula_Pilha*)malloc(sizeof(Celula_Pilha));
+    nova_celula_da_pilha->elemento = elemento;
+    nova_celula_da_pilha->proximo_ponteiro = NULL;
+
+    if(pilha_vazia(pilha))
+    {
+        pilha->topo = nova_celula_da_pilha;
+    }
+    else
+    {
+        nova_celula_da_pilha->proximo_ponteiro = pilha->topo;
+        pilha->topo = nova_celula_da_pilha;
+    }
+}
+
+Celula_Pilha* remover_elemento_da_pilha(Pilha *pilha)
+{
+    if(pilha->topo == NULL)
+    {
+        printf("Pilha Vazia\n");
+    }
+    else
+    {
+        Celula_Pilha *temporario;
+        temporario = pilha->topo;
+        pilha->topo = pilha->topo->proximo_ponteiro;
+        free(temporario);
+    }
+}
+
+void percorrer_pilha(Pilha *pilha)
+{
+    Celula_Pilha *temporario = pilha->topo;
+
+    if(pilha_vazia(pilha))
+    {
+        printf("Pilha Vazia\n");
+    }
+    else
+    {
+        while(temporario != NULL)
+        {
+            printf("Elemento: %.2f", temporario->elemento);
+            temporario = temporario->proximo_ponteiro;
+        }
+    }
+}
+
+int main()
+{
+    int valor, opcao;
+    Pilha pilha;
+
+    do
+    {
+        printf("Menu da Pilha\n1-Empilhar\n2-Desempilhar\n3-Imprimir\n0-Sair\n");
+        scanf("%d",&opcao);
+        switch(opcao)
+        {
+            case 1:
+                printf("Informe o valor a ser empilhado: ");
+                scanf("%d",&valor);
+                adicionar_elemento_na_pilha(&pilha, valor);
+                break;
+
+            case 2:
+                remover_elemento_da_pilha(&pilha);
+                break;
+            case 3:
+                percorrer_pilha(&pilha);
+                break;
+        } 
+    }
+    while(opcao != 0);
+}
